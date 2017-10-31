070a81a0cb001d2acdd8a85326d372ece68b3978
Marko Srdanovic <marko.srdanovic93@gmail.com>
2017-10-31 10:04:18 +0100

    Update ViewController.swift

--- Neki kod/Neki kod/ViewController.swift
+++ Neki kod/Neki kod/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Marko napisao ovu poruku")
+        print("Marko napisao ovu poruku") //daj ne zezaj da je tacno
     }
 
     override func didReceiveMemoryWarning() {
