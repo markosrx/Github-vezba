9b710c380d852b59b2b09590cb83acfd2549a199
Marko Srdanovic <markosrdanovic@Markos-iMac.local>
2017-10-31 09:44:00 +0100

    Ovo je neki prvi commit by srx...

---
+++ Neki kod/Neki kod/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Neki kod
+//
+//  Created by Marko Srdanovic on 10/31/17.
+//  Copyright © 2017 Marko Srdanovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Marko napisao ovu poruku")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
