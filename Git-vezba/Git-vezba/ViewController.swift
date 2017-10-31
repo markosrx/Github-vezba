8241bbc4f81e52137b473b53b4dca9f38bcf395e
Click <click@Clicks-iMac.local>
2017-10-31 09:43:27 +0100

    Lazar commit

---
+++ Git-vezba/Git-vezba/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Git-vezba
+//
+//  Created by Click on 10/31/17.
+//  Copyright © 2017 Click. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+     //   Komentar je sranje
+        //asd;laskd;laksl;
+        //asfkadjlkfa
+        super.addChildViewController(<#T##childController: UIViewController##UIViewController#>);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
